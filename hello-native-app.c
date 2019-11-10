23d433c3041c30b09aa045ee1427f948cbbdbe0f
Katayma Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-11-10 17:04:18 +0900

    ...

--- hello-native-app.c
+++ hello-native-app.c
@@ -1,8 +1,6 @@
 // hello-native-app.c
 // Copyright (C) 2019 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>.
 // This file is public domain software.
-//
-// NOTE: Copy this folder to $(REACTOS)\base\system and modify CMakeLists.txt.
 
 #define WIN32_NO_STATUS
 #include <windef.h>
